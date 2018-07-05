da8f6db7ba78c925f3514a8059cbb5307b12b46f
licheng <licheng_xd@163.com>
2018-07-05 09:44:01 +0800

    kafka collector

--- consumer/src/main/java/com/lchml/test/dubbo/sleuth/TestController.java
+++ consumer/src/main/java/com/lchml/test/dubbo/sleuth/TestController.java
@@ -2,24 +2,43 @@ package com.lchml.test.dubbo.sleuth;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.lchml.test.dubbo.sleuth.api.TestService;
+import com.lchml.test.dubbo.sleuth.api.TestService2;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * Created by lc on 2018/06/28.
  */
 @RestController
-public class TestController {
+public class TestController extends AbstractController {
+
+    private LocalBean localBean;
 
     public TestController() {
         System.out.println("init test controller");
+        localBean = new LocalBean();
     }
 
     @Reference(check = false, version = "1.0.0")
     private TestService testService;
 
+    @Reference(check = false, version = "1.0.0")
+    private TestService2 testService2;
+
+    @Resource
+    private TracingTest tracingTest;
+
+
     @RequestMapping("/")
     public String getTime() {
+        System.out.println(localBean.test());
         return testService.getTime();
+    }
+
+    @RequestMapping("/2")
+    public String getTime2() {
+        return testService2.getTime2();
     }
 }
--- provider/src/main/java/com/lchml/test/dubbo/sleuth/TestServiceImpl.java
+++ provider/src/main/java/com/lchml/test/dubbo/sleuth/TestServiceImpl.java
@@ -1,10 +1,10 @@
 package com.lchml.test.dubbo.sleuth;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.lchml.test.dubbo.sleuth.api.TestService;
+import com.lchml.test.dubbo.sleuth.api.TestService2;
 import org.springframework.stereotype.Component;
-
-import java.util.Date;
 
 /**
  * Created by lc on 2018/06/27.
@@ -16,12 +16,17 @@ import java.util.Date;
 @Component
 public class TestServiceImpl implements TestService {
 
-    public TestServiceImpl() {
-        System.out.println("TestServiceImpl init ....................");
-    }
+    @Reference(check = false, version = "1.0.0")
+    private TestService2 testService2;
 
     @Override
     public String getTime() {
-        return new Date().toString();
+        try {
+            System.out.println("sleep 100 ms ......");
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+
+        }
+        return testService2.getTime2();
     }
 }
